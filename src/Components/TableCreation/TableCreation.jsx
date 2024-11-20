f2eb870f38ebab109657febe16d05c5e44485f06
Лядов Максим <lyadov.maxim2018@yandex.ru>
2024-11-20 16:21:50 +0500

    feat: Добавил тест api

--- src/Components/TableCreation/TableCreation.jsx
+++ src/Components/TableCreation/TableCreation.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Dropdown } from "primereact/dropdown";
 import { Button } from "primereact/button";
 import "./TableCreation.css";
 
 const TableCreation = () => {
+  const [tableTemplateData, setTableTemplateData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/database/structure");
+        const result = await response.json();
+        setTableTemplateData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(tableTemplateData);
+
   let fieldsForTable = ["name", "price", "quantity"];
 
   const userTableOptions = [
